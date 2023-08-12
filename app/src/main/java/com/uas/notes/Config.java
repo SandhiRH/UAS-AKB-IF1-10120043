2cb6acfdcb611be3ff9816b9063fdd3e874d06fc
SandhiRH <sandhirahim01@gmail.com>
2023-08-12 21:11:32 +0700

    update firebase

--- app/src/main/java/com/uas/notes/Config.java
+++ app/src/main/java/com/uas/notes/Config.java
@@ -7,7 +7,7 @@ package com.uas.notes;
 
 public class Config {
 
-    public static String DB_URL = "https://uas-azhalia-default-rtdb.asia-southeast1.firebasedatabase.app/";
+    public static String DB_URL = "https://uas-10120043-default-rtdb.asia-southeast1.firebasedatabase.app/";
 
     public static String getDB_URL() {
         return DB_URL;
